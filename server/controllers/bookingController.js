50b05b5246b3062efeb2e0b5a7c332ed4856ee3c
HiwotAddis <addishiwot963@gmail.com>
2025-10-22 22:47:43 +0300

    creating APIs

---
+++ server/controllers/bookingController.js
@@ -0,0 +1,32 @@
+import Booking from "../models/Booking";
+
+// function to check availability of room
+const checkAvailability = async ({ checkInDate, checkOutDate, room }) => {
+  try {
+    const bookings = await Booking.find({
+      room,
+      checkInDate: { $lte: checkOutDate },
+      checkOutDate: { $gte: checkInDate },
+    });
+    const isAvailable = bookings.length === 0;
+    return isAvailable;
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+//API to check availability of a room
+// POST /api/bookings/check-availability
+export const checkAvailabilityAPI = async (req, res) => {
+  try {
+    const { room, checkInDate, checkOutDate } = req.body;
+    const isAvailable = await checkAvailability({
+      room,
+      checkInDate,
+      checkOutDate,
+    });
+    res.json({ success: true, isAvailable });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
