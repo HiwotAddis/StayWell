d9fe72d68e9b1b2415c70e0c2ef49b9165e1f8d2
HiwotAddis <addishiwot963@gmail.com>
2025-10-23 14:14:34 +0300

    integration

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,4 +1,6 @@
-import Booking from "../models/Booking";
+import Booking from "../models/Booking.js";
+import Hotel from "../models/Hotel.js";
+import Room from "../models/Room.js";
 
 // function to check availability of room
 const checkAvailability = async ({ checkInDate, checkOutDate, room }) => {
@@ -28,5 +30,90 @@ export const checkAvailabilityAPI = async (req, res) => {
     res.json({ success: true, isAvailable });
   } catch (error) {
     res.json({ success: false, message: error.message });
+  }
+};
+
+// API to create a new booking
+// POST /api/bookings/book
+
+export const createBooking = async (req, res) => {
+  try {
+    const { room, guests, checkInDate, checkOutDate } = req.body;
+    const user = req.user._id;
+    //before booking check avaliability
+
+    const isAvailable = await checkAvailability({
+      room,
+      checkInDate,
+      checkOutDate,
+    });
+    if (!isAvailable) {
+      return res.json({ success: false, message: "Room is not available " });
+    }
+
+    // Get totalprice from Room
+    const roomData = await Room.findById(room).populate("hotel");
+    let totalprice = roomData.pricePerNight;
+    // calculate totlaprice based on nights
+    const checkIn = new Date(checkInDate);
+    const checkOut = new Date(checkOutDate);
+    const timeDiff = checkOut.getTime() - checkIn.getTime();
+    const nights = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    totalprice *= nights;
+
+    const booking = await Booking.create({
+      room,
+      user,
+      hotel: roomData.hotel._id,
+      guests: +guests,
+      checkInDate,
+      checkOutDate,
+      totalprice,
+    });
+    res.json({ success: true, message: "Booking created successfully" });
+  } catch (error) {
+    console.error(error.message);
+    res.json({ success: false, message: "Failed to create booking" });
+  }
+};
+
+// API to get all bookings for a user
+// GET /api/bookings/user
+export const getUserBookings = async (req, res) => {
+  try {
+    const user = req.user._id;
+    const bookings = await Booking.find({ user })
+      .populate("room hotel")
+      .sort({ createdAt: -1 });
+    res.json({ success: true, bookings });
+  } catch (error) {
+    console.error(error.message);
+    res.json({ success: false, message: "Failed to fetch bookings" });
+  }
+};
+
+export const getHotelBookings = async (req, res) => {
+  try {
+    const hotel = await Hotel.findOne({ owner: req.auth.user._id });
+    if (!hotel) {
+      return res.json({ success: false, message: "No hotel found" });
+    }
+    const bookings = await Booking.find({ hotel: hotel._id })
+      .populate("room hotel user")
+      .sort({ createdAt: -1 });
+    // total bookings
+    const totalBookings = bookings.length;
+    //total revenue
+    const totalRevenue = bookings.reduce(
+      (acc, booking) => acc + booking.totalprice,
+      0
+    );
+    res.json({
+      success: true,
+      dashboardData: { bookings, totalBookings, totalRevenue },
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.json({ success: false, message: "Failed to fetch bookings" });
   }
 };
--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -1,16 +1,16 @@
 import express from "express";
-import upload from "../middleware/uploadMiddleware";
-import { protect } from "../middleware/authMiddleware";
+import upload from "../middleware/uploadMiddleware.js";
+import { protect } from "../middleware/authMiddleware.js";
 import {
   createRoom,
   getOwnerRooms,
   getRooms,
   toggleRoomAvailability,
-} from "../controllers/roomController";
+} from "../controllers/roomController.js";
 
 const roomRouter = express.Router();
 
-roomRouter.post("/", upload.array("images"), 4, protect, createRoom);
+roomRouter.post("/", upload.array("images", 4), protect, createRoom);
 roomRouter.get("/", getRooms);
 roomRouter.get("/owner", protect, getOwnerRooms);
 roomRouter.post("/toggle-availability", protect, toggleRoomAvailability);
